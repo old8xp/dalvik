336706af1c30c43fc339b92963cb90f4b9782341
enh@google.com <enh@google.com@12c02be0-ffb4-11de-a6f0-ed93ac2c91d0>
2010-07-09 21:36:45 +0000

    benchmarks/ArrayCopyBenchmark.java: more ways to copy arrays.

--- trunk/benchmarks/ArrayCopyBenchmark.java
+++ trunk/benchmarks/ArrayCopyBenchmark.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2010 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,31 +20,38 @@ import com.google.caliper.Param;
 import com.google.caliper.Runner;
 import com.google.caliper.SimpleBenchmark;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.LinkedHashMap;
+import java.util.Arrays;
 
-/**
- * What's the difference between a manual loop and System.arraycopy?
- */
 public class ArrayCopyBenchmark extends SimpleBenchmark {
-    public void timeArrayCopyManual(int reps) {
+    public void timeManualArrayCopy(int reps) {
         char[] src = new char[8192];
-        char[] dst = new char[8192];
         for (int rep = 0; rep < reps; ++rep) {
+            char[] dst = new char[8192];
             for (int i = 0; i < 8192; ++i) {
                 dst[i] = src[i];
             }
         }
     }
-    public void timeArrayCopySystem(int reps) {
+
+    public void time_System_arrayCopy(int reps) {
         char[] src = new char[8192];
-        char[] dst = new char[8192];
         for (int rep = 0; rep < reps; ++rep) {
+            char[] dst = new char[8192];
             System.arraycopy(src, 0, dst, 0, 8192);
+        }
+    }
+
+    public void time_Arrays_copyOf(int reps) {
+        char[] src = new char[8192];
+        for (int rep = 0; rep < reps; ++rep) {
+            char[] dst = Arrays.copyOf(src, 8192);
+        }
+    }
+
+    public void time_Arrays_copyOfRange(int reps) {
+        char[] src = new char[8192];
+        for (int rep = 0; rep < reps; ++rep) {
+            char[] dst = Arrays.copyOfRange(src, 0, 8192);
         }
     }
 }
