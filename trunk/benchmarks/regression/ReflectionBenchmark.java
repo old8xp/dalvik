dedebee624ec5045658371c95b22f4689eb535f4
enh@google.com <enh@google.com@12c02be0-ffb4-11de-a6f0-ed93ac2c91d0>
2010-11-03 21:25:26 +0000

    benchmarks/regression/ReflectionBenchmark.java: fix bug.

--- trunk/benchmarks/regression/ReflectionBenchmark.java
+++ trunk/benchmarks/regression/ReflectionBenchmark.java
@@ -101,7 +101,7 @@ public class ReflectionBenchmark extends SimpleBenchmark {
 
     public void timeMethod_invokeStaticV(int reps) throws Exception {
         Class<?> klass = C.class;
-        Method m = klass.getDeclaredMethod("m");
+        Method m = klass.getDeclaredMethod("sm");
         C instance = new C();
         for (int rep = 0; rep < reps; ++rep) {
             m.invoke(null);
