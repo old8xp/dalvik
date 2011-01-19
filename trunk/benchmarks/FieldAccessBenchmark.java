09fc52e29e2306f9a74a869b95286b63e841fe52
enh@google.com <enh@google.com@12c02be0-ffb4-11de-a6f0-ed93ac2c91d0>
2011-01-19 00:54:25 +0000

    benchmarks/FieldAccessBenchmark.java: (mostly) bust another myth. In >= gingerbread, field access via a synthetic access$ method is only 2x regular field access.

--- trunk/benchmarks/FieldAccessBenchmark.java
+++ trunk/benchmarks/FieldAccessBenchmark.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2010 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,6 +24,12 @@ import com.google.caliper.SimpleBenchmark;
  * What does field access cost?
  */
 public class FieldAccessBenchmark extends SimpleBenchmark {
+    private class Inner {
+        public int publicInnerIntVal;
+        protected int protectedInnerIntVal;
+        private int privateInnerIntVal;
+        int packageInnerIntVal;
+    }
     int intVal = 42;
     final int finalIntVal = 42;
     static int staticIntVal = 42;
@@ -61,6 +67,38 @@ public class FieldAccessBenchmark extends SimpleBenchmark {
         int cachedIntVal = this.intVal;
         for (int rep = 0; rep < reps; ++rep) {
             result = cachedIntVal;
+        }
+        return result;
+    }
+    public int timeFieldPrivateInnerClassPublicField(int reps) {
+        int result = 0;
+        Inner inner = new Inner();
+        for (int rep = 0; rep < reps; ++rep) {
+            result = inner.publicInnerIntVal;
+        }
+        return result;
+    }
+    public int timeFieldPrivateInnerClassProtectedField(int reps) {
+        int result = 0;
+        Inner inner = new Inner();
+        for (int rep = 0; rep < reps; ++rep) {
+            result = inner.protectedInnerIntVal;
+        }
+        return result;
+    }
+    public int timeFieldPrivateInnerClassPrivateField(int reps) {
+        int result = 0;
+        Inner inner = new Inner();
+        for (int rep = 0; rep < reps; ++rep) {
+            result = inner.privateInnerIntVal;
+        }
+        return result;
+    }
+    public int timeFieldPrivateInnerClassPackageField(int reps) {
+        int result = 0;
+        Inner inner = new Inner();
+        for (int rep = 0; rep < reps; ++rep) {
+            result = inner.packageInnerIntVal;
         }
         return result;
     }
